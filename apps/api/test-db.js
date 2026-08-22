f09d61e3043b6c93e1282d4f5eae193a3987a2d6
jeepooly-blip <jeepooly@gmail.com>
2026-08-22 21:53:10 +0300

    chore: remove test-db.js

--- apps/api/test-db.js
+++
@@ -1,5 +0,0 @@
-const { PrismaClient } = require('@prisma/client');
-const p = new PrismaClient();
-p.$queryRawUnsafe('SELECT 1 as test')
-  .then(r => { console.log('DB OK:', JSON.stringify(r)); p.$disconnect(); })
-  .catch(e => { console.error('DB ERROR:', e.message); p.$disconnect(); });
